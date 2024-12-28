235090469035385278a22b841286fca87b541137
furkan-aydin <furkanaydin.gtu@gmail.com>
2024-12-28 23:26:25 +0300

    Initial commit

---
+++ src/main/java/com/elasticdata/example/kafkaproducer/KafkaProducerConfig.java
@@ -0,0 +1,12 @@
+package com.elasticdata.example.kafkaproducer;
+
+
+@Configuration
+public class KafkaProducerConfig {
+
+//    @Bean
+//    public ProducerFactory <String,String> producerFactory() {
+//
+//    }
+
+}
